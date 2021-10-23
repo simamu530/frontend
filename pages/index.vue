16d1dffb03aefe03125fe98c7064d34a3d235059
simamu530 <58772879+simamu530@users.noreply.github.com>
2021-10-23 13:22:30 +0900

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -493,6 +493,21 @@ export default {
 
     deleteItemConfirm () {
       this.items.splice(this.editedIndex, 1)
+      const testid = Object.assign(this.items[this.editedIndex].id);
+        console.log(Object.assign(this.items[this.editedIndex].id));
+        console.log(Object.assign(this.items[this.editedIndex], this.editedItem));
+        Object.assign(this.items[this.editedIndex], this.editedItem)
+        const sendData = Object.assign(this.items[this.editedIndex])
+        try {
+          this.$axios.delete('/api/v1/apexprolist/' + testid, sendData)
+          .then( res => {
+            console.log(res);
+            console.log("success");
+          }
+          )
+        } catch (error) {
+          console.log(error);
+        }
       this.closeDelete()
     },
 
