ff2b915714c72768e5feacae678f5cf5b09da4cb
simamu530 <58772879+simamu530@users.noreply.github.com>
2022-01-29 14:23:24 +0900

    firebaseJS

---
+++ plugins/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase'
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(
+    {
+      apiKey: "AIzaSyDcvMYHVcO-zjbdsclIEnIGnAfFQ4ay68o",
+      authDomain: "prosettinglist.firebaseapp.com",
+      projectId: "prosettinglist",
+      storageBucket: "prosettinglist.appspot.com",
+      messagingSenderId: "878411826876",
+      appId: "1:878411826876:web:904d21e0c45e3b9f50f2b9",
+      measurementId: "G-M0W1HY4SWF"
+    }
+  )
+}
+  
+export default firebase
