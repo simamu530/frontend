84e23c3fa0f4cfa0e8d775d99e04f6a38d8a021e
simamu530 <58772879+simamu530@users.noreply.github.com>
2022-04-13 12:36:14 +0900

    4.13
    ログイン情報分岐

---
+++ plugins/AuthProvider.js
@@ -0,0 +1,48 @@
+import firebase from "firebase/app";
+import { createContext, useEffect, useState, VFC, ReactNode } from "react";
+import { auth, db } from "../firebase";
+export const User = firebase.User;
+
+const AuthContextProps = {
+  currentUser: User | null | undefined;
+  signInCheck: boolean;
+};
+
+const AuthContext = createContext<AuthContextProps>({
+  currentUser: undefined,
+  signInCheck: false,
+});
+
+
+const AuthProvider: VFC<Props> = ({ children }) => {
+  const [currentUser, setCurrentUser] =
+    useState<User | null | undefined>(undefined);
+
+  const [signInCheck, setSignInCheck] = useState(false);
+   
+   // ログイン状態を確認する
+  
+  mounted() {
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        console.log('login');
+      } else {
+        console.log('logout');
+      }
+    });     
+  }
+
+  if (signInCheck) {
+    return (
+      <AuthContext.Provider value={{ currentUser, signInCheck }}>
+        {children}
+      </AuthContext.Provider>
+    );
+  } else {
+  　　　 // ログイン確認中
+    // 自分で作ったローディングコンポーネントをレンダリングする
+    return <Index />;
+  }
+};
+
+export { AuthContext, AuthProvider };
