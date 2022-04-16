dd102e044ed27bd801f7be63caadad0239beeb77
simamu530 <58772879+simamu530@users.noreply.github.com>
2022-04-17 04:23:05 +0900

    Update auth-display.js

--- plugins/auth-display.js
+++ plugins/auth-display.js
@@ -8,7 +8,13 @@ firebase.auth().onAuthStateChanged((user) => {
     user.getIdTokenResult(true).then((idTokenResult) => {
       if (idTokenResult.claims.admin) {
         // current user has admin
+        //4/16　おそらくここに管理者権限が使用できる機能を追記
+      }
+      else {
+        //ここにログイン済みユーザーが使用できる機能を追記
       }
     });
+  } else {
+    //未登録もしくは未ログインユーザーが使用できる機能を追記
   }
 });
