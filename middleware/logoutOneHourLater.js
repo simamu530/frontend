d99fa7bfb7e37517e03c1f056115ba9276d7f859
simamu530 <58772879+simamu530@users.noreply.github.com>
2022-09-08 10:50:09 +0900

    Update logoutOneHourLater.js

--- middleware/logoutOneHourLater.js
+++ middleware/logoutOneHourLater.js
@@ -1,8 +1,11 @@
-let dt = new Date();
-let ts = dt.getTime();
-let ts_after = ts + 1000 * 60 * 60 * 1;
+export default function (context) {
 
-if (ts) {
-  ts !== ts_after;
-  
+  let dt = new Date();
+  let ts = dt.getTime();
+  let ts_after = ts + 1000 * 60 * 60 * 1;
+
+  if (ts) {
+    ts !== ts_after;
+    
+  }
 }
