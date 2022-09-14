95e4ed6d1247bf20732b67d1004d2de9cc37ec6e
simamu530 <58772879+simamu530@users.noreply.github.com>
2022-09-14 19:51:07 +0900

    Update logoutOneHourLater.js

--- middleware/logoutOneHourLater.js
+++ middleware/logoutOneHourLater.js
@@ -1,11 +1,6 @@
 export default function ({ redirect, store, route }) {
-  const user = store.state.user
-  if(!user && route.path !== '/login') {
-    redirect('/login')
-  }
-  if (route.path !== '/login') {
-    redirect('/login')
-  }
+  // const user = store.state.user
+  if (route.path !== '/login') return
 
   //storeに保存してる内容
   let getExpired = store.state.dbState.expired;
